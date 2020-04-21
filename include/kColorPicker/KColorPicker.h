1926d5a67c76135c8e4752005cb1bfe17ba737ec
Damir Porobic <damir_porobic@live.com>
2020-04-21 15:20:13 +0200

    Use upper case qt keywords for exported header file

--- include/kColorPicker/KColorPicker.h
+++ include/kColorPicker/KColorPicker.h
@@ -43,20 +43,21 @@ public:
 	void setFixedSize(int width, int height);
 	QColor color() const;
 
-public slots:
+public Q_SLOTS:
 	void setColor(const QColor &color);
 
-signals:
+Q_SIGNALS:
 	void colorChanged(const QColor &color) const;
 
 private:
 	QScopedPointer<KColorPickerPrivate> const d_ptr;
 
-private slots:
+private Q_SLOTS:
 	void setColorIcon(const QColor &color);
 	void setIconSize(const QSize &size);
 	void colorSelected(const QColor &color);
 };
 
 }
+
 #endif //KCOLORPICKER_KCOLORPICKER_H
